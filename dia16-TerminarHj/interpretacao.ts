96a9281fbd5c54d12ce775b6be57029336a44ebb
Henrique <143638917+Henrique10s@users.noreply.github.com>
2023-10-16 14:31:27 -0300

    Add files via upload

---
+++ dia16-TerminarHj/interpretacao.ts
@@ -0,0 +1,31 @@
+// 1- A sintaxe basica do while é : 
+
+
+/* while (condição) {
+  rotina
+}
+*/
+
+
+// 2 -  exemplo;
+
+/* let menu = true
+
+   while(menu){
+    let opcao = leitor.question("Como foi o  seu dia?")
+
+    switch(opcao){
+        case 1: leitor.question("Digite 1 para foi bom!")
+        break;
+        case 2: leitor.question("Digite 2 para foi ruim!")
+        break
+        case 3: leitor.question("Digite para sair!")
+        break
+        default;
+        menu = false
+
+    }
+   }
+*/
+
+// 3 - 
